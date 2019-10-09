e3214d6649ee7c3ec84b2be3a150c2ed6860c4a7
siramk <siramk@iitbhilai.ac.in>
2019-10-09 22:29:21 +0530

    added main-menu(with small bug)

---
+++ index.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{ int i,j,type_of_op,crypt_type,object_type,no_of_algos,no_of_objects;
+  char algos[3][15]={"ceaser cipher","AES","RSA"};
+  char objects[3][10]={"string","file","folder"};
+  no_of_algos=sizeof(algos)/sizeof(algos[0]);
+  no_of_objects=sizeof(objects)/sizeof(objects[0]);
+  /*
+  type of operations
+    1->encryption
+    2->decryption
+
+encryption or decryptions types
+    1->ceaser cipher
+    2->aes
+    3->rsa
+type of objects
+  1->string
+  2->file
+  3->folder
+
+  */
+printf("choose one of the following operations to perform:\n");
+printf("1->encryption\n2->decryption\n");
+while(1)
+{
+    printf("INPUT: ");
+    scanf("%d",&type_of_op);
+    if(type_of_op>=1 && type_of_op<=2)
+      break;
+    else
+      printf("\n!!!! PLEASE ENTER CORRECT OPTION NUMBER !!!!!\n");
+}
+
+
+
+printf("\n");
+printf("choose any one cryptographic method from the following:\n");
+for(i=0;i<no_of_algos;i++)
+{
+  printf("%d->%s\n",i+1,algos[i]);
+}
+printf("\n");
+while(1)
+{
+    printf("INPUT: ");
+    scanf("%d",&crypt_type);
+    if(crypt_type>=1 && crypt_type<=no_of_algos)
+      break;
+    else
+      printf("\n!!!! PLEASE ENTER CORRECT OPTION NUMBER !!!!!\n");
+
+}
+printf("\n");
+printf("choose one type of object from the following:\n");
+for(i=0;i<no_of_objects;i++)
+{
+  printf("%d->%s\n",i+1,objects[i]);
+}
+printf("\n");
+while(1)
+{
+    printf("INPUT: ");
+    scanf("%d",&object_type);
+    if(object_type>=1 && object_type<=no_of_objects)
+      break;
+    else
+      printf("\n!!!! PLEASE ENTER CORRECT OPTION NUMBER !!!!!\n");
+
+}
+
+  return 0;
+}
