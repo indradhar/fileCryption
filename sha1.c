b11f2d8b54048d92f868a6bb3396b6cbc062bd5c
manu156 <manu156@users.noreply.github.com>
2019-10-10 20:03:17 +0530

    added code

--- sha1.c
+++ sha1.c
@@ -1,1 +1,23 @@
-//To-do
+/*
+openssl library is required.
+For ubuntu it can be installed with: sudo apt-get install libssl-dev
+compiling: $gcc sha1.c -lssl -lcrypto
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/sha.h>
+ 
+int main()
+{
+  int i;
+  unsigned char result[SHA_DIGEST_LENGTH];
+  const char *string = "Rosetta Code";
+ 
+  SHA1(string, strlen(string), result);
+ 
+  for(i = 0; i < SHA_DIGEST_LENGTH; i++)
+    printf("%02x%c", result[i], i < (SHA_DIGEST_LENGTH-1) ? ' ' : '\n');
+ 
+  return EXIT_SUCCESS;
+}
