af96b075240b1e6059296dd7185519c8012708b7
siramk <siramk@iitbhilai.ac.in>
2019-10-11 22:28:12 +0530

    bug fix in header.h

--- header.h
+++ header.h
@@ -5,7 +5,7 @@ void shuffle(char *array, size_t n);
 void ma_encrypt_string(char *str,char *encryption_key);
 void ma_cipher_encrypt(int object_type);
 void ma_decrypt_string(char *buffer,char *decryption_key);
-void ma_cipher_decrypt(int object_type,char *encryption_key);
+void ma_cipher_decrypt(int object_type);
 int randNumber(int lower,int upper);
 void keyFile_encrypt_string(char *str);
 void keyFile_encrypt_file(char *file_name);
