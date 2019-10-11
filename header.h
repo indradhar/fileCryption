1db3cee792a9cca5984c400324e46a3fb6addd3c
siramk <siramk@iitbhilai.ac.in>
2019-10-11 18:27:09 +0530

    added function declarations of ma_cipher in header.h

--- header.h
+++ header.h
@@ -1,3 +1,8 @@
 
 void stringHash(char *string);
 void getFileHash(char *fileName);
+void shuffle(char *array, size_t n);
+void ma_encrypt_string(char *str,char *encryption_key);
+void ma_cipher_encrypt(int object_type);
+void ma_decrypt_string(char *buffer,char *decryption_key);
+void ma_cipher_decrypt(int object_type,char *encryption_key);
