e2f48fc9c04c842a53f8052f0327158fc1914f0c
manu156 <manu156@users.noreply.github.com>
2019-10-11 13:31:21 +0530

    added hash for string and file

--- sha1.c
+++ sha1.c
@@ -8,16 +8,47 @@ compiling: $gcc sha1.c -lssl -lcrypto
 #include <string.h>
 #include <openssl/sha.h>
  
-int main()
-{
+
+void stringHash(char *string) {
   int i;
   unsigned char result[SHA_DIGEST_LENGTH];
-  const char *string = "Rosetta Code";
- 
   SHA1(string, strlen(string), result);
- 
   for(i = 0; i < SHA_DIGEST_LENGTH; i++)
     printf("%02x%c", result[i], i < (SHA_DIGEST_LENGTH-1) ? ' ' : '\n');
- 
-  return EXIT_SUCCESS;
+}
+
+void getFileHash(char *fileName){
+
+unsigned char result[2*SHA_DIGEST_LENGTH];
+unsigned char hash[SHA_DIGEST_LENGTH];
+int i;
+FILE *f = fopen(fileName,"rb");
+SHA_CTX mdContent;
+int bytes;
+unsigned char data[1024];
+
+if(f == NULL){
+    printf("%s couldn't open file\n",fileName);
+    exit(1);
+}
+
+SHA1_Init(&mdContent);
+while((bytes = fread(data, 1, 1024, f)) != 0){
+
+    SHA1_Update(&mdContent, data, bytes);
+}
+
+SHA1_Final(hash,&mdContent);
+
+for(i=0;i<SHA_DIGEST_LENGTH;i++){
+    printf("%02x",hash[i]);
+}
+printf("\n");
+/** if you want to see the plain text of the hash */
+//for(i=0; i < SHA_DIGEST_LENGTH;i++){
+//    sprintf((char *)&(result[i*2]), "%02x",hash[i]);
+//}
+//printf("%s\n",result);
+
+fclose(f);
 }
