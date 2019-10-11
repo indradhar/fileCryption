6a70562db0155812564a10147ac441466ec9d096
manu156 <manu156@users.noreply.github.com>
2019-10-11 14:02:43 +0530

    removed folder option

--- index.c
+++ index.c
@@ -1,31 +1,33 @@
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
+
 int main()
-{ int i,j,type_of_op,crypt_type,object_type,no_of_algos,no_of_objects;
+{
+  int i,j,type_of_op,crypt_type,object_type,no_of_algos,no_of_objects;
   char algos[3][15]={"ceaser cipher","AES","RSA"};
-  char objects[3][10]={"string","file","folder"};
+  char objects[2][10]={"string","file"};
   no_of_algos=sizeof(algos)/sizeof(algos[0]);
   no_of_objects=sizeof(objects)/sizeof(objects[0]);
+  
   /*
   type of operations
     1->encryption
     2->decryption
 
-encryption or decryptions types
+  encryption or decryptions types
     1->ceaser cipher
     2->aes
     3->rsa
-type of objects
-  1->string
-  2->file
-  3->folder
+  type of objects
+    1->string
+    2->file
 
   */
-printf("choose one of the following operations to perform:\n");
-printf("1->encryption\n2->decryption\n");
-while(1)
-{
+  printf("choose one of the following operations to perform:\n");
+  printf("1->encryption\n2->decryption\n");
+  while(1)
+  {
     printf("INPUT: ");
     if(scanf("%d",&type_of_op)==0)
       getchar();
@@ -33,19 +35,19 @@ while(1)
       break;
     else
       printf("\n!!!! PLEASE ENTER CORRECT OPTION NUMBER !!!!!\n");
-}
+  }
 
 
 
-printf("\n");
-printf("choose any one cryptographic method from the following:\n");
-for(i=0;i<no_of_algos;i++)
-{
-  printf("%d->%s\n",i+1,algos[i]);
-}
-printf("\n");
-while(1)
-{
+  printf("\n");
+  printf("choose any one cryptographic method from the following:\n");
+  for(i=0;i<no_of_algos;i++)
+  {
+   printf("%d->%s\n",i+1,algos[i]);
+  }
+  printf("\n");
+  while(1)
+  {
     printf("INPUT: ");
     if(scanf("%d",&crypt_type)==0)
       getchar();
@@ -55,16 +57,16 @@ while(1)
     else
       printf("\n!!!! PLEASE ENTER CORRECT OPTION NUMBER !!!!!\n");
 
-}
-printf("\n");
-printf("choose one type of object from the following:\n");
-for(i=0;i<no_of_objects;i++)
-{
+  }
+  printf("\n");
+  printf("choose one type of object from the following:\n");
+  for(i=0;i<no_of_objects;i++)
+  {
   printf("%d->%s\n",i+1,objects[i]);
-}
-printf("\n");
-while(1)
-{
+  }
+  printf("\n");
+  while(1)
+  {
     printf("INPUT: ");
     if(scanf("%d",&object_type)==0)
       getchar();
@@ -74,7 +76,7 @@ while(1)
     else
       printf("\n!!!! PLEASE ENTER CORRECT OPTION NUMBER !!!!!\n");
 
-}
+  }
 
   return 0;
 }
