489feb03380c23b62e78eecb7757ac875302e5a3
siramk <siramk@iitbhilai.ac.in>
2019-10-11 12:48:51 +0530

    added decryption functionality to mono_alphabaetic_cipher.c

--- mono_alphabetic_cipher.c
+++ mono_alphabetic_cipher.c
@@ -60,8 +60,8 @@ void ma_cipher_encrypt(int object_type)
     case 2:
           printf("please enter a path(absolute) to required file:\nINPUT: ");
           fgets(file_name,MAX_LIMIT,stdin);
-          fs=fopen(file_name,'r');
-          fd=fopen("dest.txt",'w');
+          fs=fopen(file_name,"r");
+          fd=fopen("dest.txt","w");
           if(fd==NULL || fs==NULL)
           {
             printf("ERROR:unable to do file handling!!");
@@ -81,4 +81,55 @@ void ma_cipher_encrypt(int object_type)
           }
 
   }
+}
+
+void ma_decrypt_string(char *buffer,char *decryption_key)
+{   int i;
+    for(i=0;i<strlen(buffer);i++)
+    {
+        if(buffer[i]>='A' && buffer[i]<='Z')
+        {
+          buffer[i]=decryption_key[buffer[i]-'A'];
+        }
+    }
+}
+
+void ma_cipher_decrypt(int object_type,char *encryption_key)
+{
+  char decryption_key[26],str[MAX_LIMIT],file_name[MAX_LIMIT],buffer[1025];
+  int i,idx;
+  FILE *fs,*fd;
+  for(i=0;i<26;i++)
+  {
+    idx=encryption_key[i]-'A';
+    decryption_key[idx]='a'+i;
+  }
+  switch(object_type)
+  {
+    case 1:
+            printf("please enter the encrypted string to decrypt! \n INPUT: ");
+            fgets(str,MAX_LIMIT-1,stdin);
+            ma_decrypt_string(str,decryption_key);
+    case 2:
+            printf("please enter the file name that is to decrypted\nINPUT: ");
+            fgets(file_name,MAX_LIMIT-1,stdin);
+            fs=fopen(file_name,"r");
+            fd=fopen("dest.txt","w");
+            if(fs==NULL || fd==NULL)
+            {
+              printf("ERROR:unable to perform file handling!!\n");
+              exit(0);
+            }
+            while(fgets(buffer,1024,fs)!=NULL)
+            {
+              ma_decrypt_string(buffer,decryption_key);
+              fputs(buffer,fd);
+            }
+            fclose(fs);
+            fclose(fd);
+            remove(file_name);
+            rename("dest.txt",file_name);
+
+  }
+
 }
